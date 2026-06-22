d6209425decbd2aff1b8b36426f320d06c20996c
Reham Ali <rehamalibhatti9@gmailcom>
2026-06-22 23:04:58 +0500

    first prep

---
+++ prep.js
@@ -0,0 +1,39 @@
+// const array  = [1,2,3];
+// array.push(4);
+// console.log(array);
+
+// const person = {name : "ali"};
+// person.name = "hamza";
+// console.log(person)
+//console.log(typeof null);
+//let c = 1; // declaring and assigning a vlaue to the c
+//console.log(c)
+//c = 2; // reassignging of the value of the c 
+//console.log(c);
+//let c = 3; // redeclaring and assigning the value, not acceptable at all 
+//console.log(Boolean(0));
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+// arr2.push(4);
+// console.log(arr1.length);
+
+// console.log("10" - "4"); // 6
+// console.log("10" + "4"); // 104
+// console.log(10 + "4" + 4); // 1044
+// if (null === object)
+// {
+//     console.log("fine");
+// }
+// else
+//      {
+//     console.log("not fine");
+// }
+
+
+let name = "World";
+let greeting2 = `Hello ${name}`;
+console.log(greeting2);
+let fruits = ["apple" , "anar" , "mangoes"];
+let person = {name: "ali" , age : 25};
+console.log(fruits);
+console.log(person);
